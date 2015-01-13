4c3a705bc251669b0481b239561d541e49acbc0b
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-12 20:50:57 -0500

    adds tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
- * dirname-regex <https://github.com/jonschlinkert/dirname-regex>
+ * dirname-regex <https://github.com/regexps/dirname-regex>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015 Jon Schlinkert
  * Licensed under the MIT License
  */
 
@@ -10,21 +10,86 @@
 var assert = require('assert');
 var dirname = require('./');
 
-describe('dirname regex', function () {
-  it('should match the directory with a file name:', function () {
-    var path = dirname().exec('a/b/c/d.md');
-    assert.equal(path[0], 'a/b/c/d.md');
-    assert.equal(path[1], 'a/b/c/');
-  });
-  it('should match the directory ending with a slash:', function () {
-    var path = dirname().exec('a/b/c/');
-    assert.equal(path[0], 'a/b/c/');
-    assert.equal(path[1], 'a/b/c/');
-  });
+function match(str) {
+  return str.match(dirname());
+}
 
-  it('should match the directory without a file name:', function () {
-    var path = dirname().exec('a/b/c');
-    assert.equal(path[0], 'a/b/c');
-    assert.equal(path[1], 'a/b/');
-  });
+it('should match a directory with a file name:', function () {
+  assert.equal(match('a/b/c/d.md')[0], 'a/b/c/d.md');
+  assert.equal(match('a/b/c/d.md')[1], 'a/b/c/');
+  assert.equal(match('a/b/c/d.md')[2], 'a/b/c');
+  assert.equal(match('a\\b\\c\\d.md')[0], 'a\\b\\c\\d.md');
+  assert.equal(match('a\\b\\c\\d.md')[1], 'a\\b\\c\\');
+  assert.equal(match('a\\b\\c\\d.md')[2], 'a\\b\\c');
+});
+
+it('should match a directory without a file name:', function () {
+  assert.equal(match('a/b/c')[0], 'a/b/c');
+  assert.equal(match('a/b/c')[1], 'a/b/');
+  assert.equal(match('a/b/c')[2], 'a/b');
+  assert.equal(match('a\\b\\c')[0], 'a\\b\\c');
+  assert.equal(match('a\\b\\c')[1], 'a\\b\\');
+  assert.equal(match('a\\b\\c')[2], 'a\\b');
+});
+
+it('should match a dots and slashes:', function () {
+  assert.equal(match('.')[0], '.');
+  assert.equal(match('.')[1], '.');
+  assert.equal(match('.')[2], null);
+  assert.equal(match('/')[0], '/');
+  assert.equal(match('/')[1], '/');
+  assert.equal(match('/')[2], '');
+  assert.equal(match('./')[0], './');
+  assert.equal(match('./')[1], './');
+  assert.equal(match('./')[2], '.');
+  assert.equal(match('//////')[0], '//////');
+  assert.equal(match('//////')[1], '//////');
+  assert.equal(match('//////')[2], '/////');
+  assert.equal(match('\\')[0], '\\');
+  assert.equal(match('\\')[1], '\\');
+  assert.equal(match('\\')[2], '');
+  assert.equal(match('.\\')[0], '.\\');
+  assert.equal(match('.\\')[1], '.\\');
+  assert.equal(match('.\\')[2], '.');
+});
+
+it('should return `null` if no directory:', function () {
+  assert.equal(match(''), null);
+  assert.equal(match('d.md'), null);
+});
+
+it('should match a directory ending with a slash:', function () {
+  assert.equal(match('a/b/c/')[0], 'a/b/c/');
+  assert.equal(match('a/b/c/')[1], 'a/b/c/');
+  assert.equal(match('a/b/c/')[2], 'a/b/c');
+  assert.equal(match('a\\b\\c\\')[0], 'a\\b\\c\\');
+  assert.equal(match('a\\b\\c\\')[1], 'a\\b\\c\\');
+  assert.equal(match('a\\b\\c\\')[2], 'a\\b\\c');
+});
+
+it('should match a directory starting with a slash:', function () {
+  assert.equal(match('/a/b/c/d.js')[0], '/a/b/c/d.js');
+  assert.equal(match('/a/b/c/d.js')[1], '/a/b/c/');
+  assert.equal(match('/a/b/c/d.js')[2], '/a/b/c');
+  assert.equal(match('\\a\\b\\c\\d.js')[0], '\\a\\b\\c\\d.js');
+  assert.equal(match('\\a\\b\\c\\d.js')[1], '\\a\\b\\c\\');
+  assert.equal(match('\\a\\b\\c\\d.js')[2], '\\a\\b\\c');
+});
+
+it('should match a directory starting with a dot:', function () {
+  assert.equal(match('./a/b/c/d.js')[0], './a/b/c/d.js');
+  assert.equal(match('./a/b/c/d.js')[1], './a/b/c/');
+  assert.equal(match('./a/b/c/d.js')[2], './a/b/c');
+  assert.equal(match('.\\a\\b\\c\\d.js')[0], '.\\a\\b\\c\\d.js');
+  assert.equal(match('.\\a\\b\\c\\d.js')[1], '.\\a\\b\\c\\');
+  assert.equal(match('.\\a\\b\\c\\d.js')[2], '.\\a\\b\\c');
+});
+
+it('should match a directory with dots in a non-filename segment:', function () {
+  assert.equal(match('./a/b/c/d.e.f/g.js')[0], './a/b/c/d.e.f/g.js');
+  assert.equal(match('./a/b/c/d.e.f/g.js')[1], './a/b/c/d.e.f/');
+  assert.equal(match('./a/b/c/d.e.f/g.js')[2], './a/b/c/d.e.f');
+  assert.equal(match('.\\a\\b\\c\\d.e.f\\g.js')[0], '.\\a\\b\\c\\d.e.f\\g.js');
+  assert.equal(match('.\\a\\b\\c\\d.e.f\\g.js')[1], '.\\a\\b\\c\\d.e.f\\');
+  assert.equal(match('.\\a\\b\\c\\d.e.f\\g.js')[2], '.\\a\\b\\c\\d.e.f');
 });
